8ba92dd77b84bdb1af02a6fb2b8ac8f0002e9327
Amrutha D N <amruthadn13@gmail.com>
2026-05-28 22:08:25 +0530

    Create studentData.c

---
+++ studentData.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int studentID = 05;
+    float score = 87.5;
+    char grade[] = "A+";
+
+    printf("The Student ID is : %i ", studentID);
+    printf("\nTotal Score is : %f ", score);
+    printf("\nThe Final Grade is : %s ", grade);
+
+    return 0;
+}
